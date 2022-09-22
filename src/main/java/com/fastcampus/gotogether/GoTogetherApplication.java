262549adb99aae884522a3832a17d2df5645c8b2
Woogie95 <choisw950928@gmail.com>
2022-09-22 17:35:40 +0900

    "test"

---
+++ src/main/java/com/fastcampus/gotogether/GoTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.gotogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoTogetherApplication.class, args);
+	}
+
+}
